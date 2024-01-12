ecf37b390bb4b465c27df18c2e45261a2980198e
D-Prince69 <ubaniprince805@gmail.com>
2024-01-12 01:04:00 +0100

    changed images

--- index.php
+++ index.php
@@ -8,12 +8,15 @@
   <div class="slider_container">
 
     <figure>
-      <img src="images/sliders/slider1.png" alt="">
-      <img src="images/sliders/slider2.png" alt="">
-      <img src="images/sliders/slider3.png" alt="">
-      <img src="images/sliders/slider4.png" alt="">
-      <img src="images/sliders/slider6.png" alt="">
-      <img src="images/sliders/slider1.png" alt="">
+      <img src="images/sliders/appliances 1.jpeg" alt="">
+      <img src="images/sliders/appliances 6.jpeg" alt="">
+      <img src="images/sliders/appliances 3.png" alt="">
+      <img src="images/sliders/appliances 4.jpeg" alt="">
+      <img src="images/sliders/appliances 5.jpg" alt="">
+      <img src="images/sliders/appliances 2.jpeg" alt="">
+      <img src="images/sliders/appliances 8.jpg" alt="">
+      <img src="images/sliders/appliances 7.jpeg" alt="">
+      <img src="images/sliders/appliances 1.jpeg" alt="">  
     </figure>
 
   </div>
@@ -21,30 +24,30 @@
   <div class="slider">
     <div class="slider-items">
       <div class="item active">
-        <img src="images/sliders/slider1.png" alt="">
+        <img src="images/sliders/appliances 5.jpg" alt="">
         <div class="caption">
-          Slider 1
+          <b>Slider 1</b>
         </div>
       </div>
 
       <div class="item">
-        <img src="images/sliders/slider2.png" alt="">
+        <img src="images/sliders/appliances 2.jpeg" alt="">
         <div class="caption">
-          Slider 2
+         <b> Slider 2</b>
         </div>
       </div>
 
       <div class="item">
-        <img src="images/sliders/slider3.png" alt="">
+        <img src="images/sliders/appliances 3.png" alt="">
         <div class="caption">
-          Slider 3
+          <b>Slider 3</b>
         </div>
       </div>
 
       <div class="item">
-        <img src="images/sliders/slider4.png" alt="">
+        <img src="images/sliders/appliances 4.jpeg" alt="">
         <div class="caption">
-          Slider 4
+          <b>Slider 4</b>
         </div>
       </div>
     </div>
@@ -67,45 +70,69 @@
 
     <div class="product_container">
       <div class="product">
-        <img src="images/products/ac.jpg" alt="">
-        <h4>Air Conditioner</h4>
+        <img src="images/products/blender.jpeg" alt="">
+        <h4>Blender</h4>
       </div>
 
       <div class="product">
-        <img src="images/products/fan.jpg" alt="">
+        <img src="images/products/clipper.jpeg" alt="">
+        <h4>Clipper</h4>
+      </div>
+
+      <div class="product">
+        <img src="images/products/dish washer.jpg" alt="">
+        <h4>Dish Washer</h4>
+      </div>
+      </div>
+
+      <div class="product_container">
+      <div class="product">
+        <img src="images/products/electric kettle.jpeg" alt="">
+        <h4>Electric Kettle</h4>
+      </div>
+
+      <div class="product">
+        <img src="images/products/hair drier.jpeg" alt="">
+        <h4>Hair Drier</h4>
+      </div>
+
+      <div class="product">
+        <img src="images/products/inverter.png" alt="">
+        <h4>Inverter</h4>
+      </div>
+      </div>
+      
+      <div class="product_container">
+      <div class="product">
+        <img src="images/products/lamp stand.jpeg" alt="">
+        <h4>Lamp Stands</h4>
+      </div>
+
+      <div class="product">
+        <img src="images/products/speakers.jpeg" alt="">
+        <h4>Speakers</h4>
+      </div>
+
+      <div class="product">
+        <img src="images/products/standing fan.jpeg" alt="">
         <h4>Standing Fan</h4>
       </div>
-
+      </div>
+      
+      <div class="product_container">
       <div class="product">
-        <img src="images/products/fridge.png" alt="">
-        <h4>Fridge</h4>
+        <img src="images/products/vacuum cleaner.jpeg" alt="">
+        <h4>Vacuume Cleaner</h4>
       </div>
 
       <div class="product">
-        <img src="images/products/gen.jpg" alt="">
-        <h4>Generator</h4>
-      </div>
-
-    </div>
-    <div class="product_container">
-      <div class="product">
-        <img src="images/products/iron.jpg" alt="">
-        <h4>Electric Iron</h4>
+        <img src="images/products/washing machine.jpeg" alt="">
+        <h4>Washing Machine</h4>
       </div>
 
       <div class="product">
-        <img src="images/products/microwave.jpg" alt="">
-        <h4>Microwave oven</h4>
-      </div>
-
-      <div class="product">
-        <img src="images/products/stove.jpg" alt="">
-        <h4>Electric Stove</h4>
-      </div>
-
-      <div class="product">
-        <img src="images/products/tv.jpg" alt="">
-        <h4>Plasma Tv</h4>
+        <img src="images/products/water dispenser.jpeg" alt="">
+        <h4>Water Dispenser</h4>
       </div>
 
     </div>
